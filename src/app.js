142b748cd0a5477791a5414b4d8d963637ea4c17
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-12-08 15:40:28 -0300

    alterando configurações

--- src/app.js
+++ src/app.js
@@ -7,12 +7,8 @@ app.use(express.json());
 app.use('/products', router.productsRouter);
 app.use('/sales', router.salesRouter);
 
-// não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
 module.exports = app;
